feab88104fbd37163fa4267461881ddc176fe5ad
Martha <Martha@qq.com>
2022-01-10 17:33:59 +0800

    IDEA hot-fix Second Commit

--- GitTest/src/main/java/com/martha/test/GitTest.java
+++ GitTest/src/main/java/com/martha/test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("hot-fix changes");
+        System.out.println("hot-fix another changes");
     }
 }
